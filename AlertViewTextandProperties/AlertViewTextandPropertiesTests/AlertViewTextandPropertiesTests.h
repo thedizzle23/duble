d587ac4885ac187df841f8f207d2d0c4f5d2555e
Robert DeNicola <thedizzle23@gmail.com>
2012-05-16 02:04:33 -0400

    message

---
+++ AlertViewTextandProperties/AlertViewTextandPropertiesTests/AlertViewTextandPropertiesTests.h
@@ -0,0 +1,13 @@
+//
+//  AlertViewTextandPropertiesTests.h
+//  AlertViewTextandPropertiesTests
+//
+//  Created by Robert DeNicola on 5/16/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AlertViewTextandPropertiesTests : SenTestCase
+
+@end
---
+++ AlertViewTextandProperties/AlertViewTextandPropertiesTests/AlertViewTextandPropertiesTests.m
@@ -0,0 +1,32 @@
+//
+//  AlertViewTextandPropertiesTests.m
+//  AlertViewTextandPropertiesTests
+//
+//  Created by Robert DeNicola on 5/16/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "AlertViewTextandPropertiesTests.h"
+
+@implementation AlertViewTextandPropertiesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AlertViewTextandPropertiesTests");
+}
+
+@end
