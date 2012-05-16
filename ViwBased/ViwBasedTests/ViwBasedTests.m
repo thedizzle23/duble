ab2a7c8b70d3d77613c52539f47c5dc71831d9bf
Robert DeNicola <thedizzle23@gmail.com>
2012-05-16 01:04:21 -0400

    message

---
+++ ViwBased/ViwBasedTests/ViwBasedTests.m
@@ -0,0 +1,32 @@
+//
+//  ViwBasedTests.m
+//  ViwBasedTests
+//
+//  Created by Robert DeNicola on 5/16/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "ViwBasedTests.h"
+
+@implementation ViwBasedTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ViwBasedTests");
+}
+
+@end
